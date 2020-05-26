13e7fd09f242c70be9fc378c4981af6b94f8fdb8
Juliette Abeyta <juliette.abeyta@evgo.com>
2020-05-25 19:17:28 -0700

    Fix svgs :)

--- app.vue
+++ app.vue
@@ -10,9 +10,8 @@
         <li class="list-group-item incomplete" v-for="(task,index) in tasks" v-show="!task.completed" :ref="'taskListItem'+index" v-bind:key="index + 'incomplete'">
           <div class="check-container">
             <input type="checkbox" :id="'incompleted-task-id-' + index " v-model="task.completed">
-            <svg width="96" height="96" viewBox="0 0 96 96" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M90.048 35C91.3168 39.1088 92 43.4746 92 48C92 72.3005 72.3005 92 48 92C23.6995 92 4 72.3005 4 48C4 23.6995 23.6995 4 48 4C58.0262 4 67.2692 7.35349 74.6684 13" stroke="#808080" stroke-width="8"/>
-              <path d="M24.0338 43.3349L45.007 65.4992C45.4155 65.9308 46.108 65.9131 46.4938 65.4612L87.7866 17.1036" stroke="#808080" stroke-width="10"/>
+            <svg height="20" width="20" viewBox="0 0 100 100">
+              <use xlink:href="/checkcircle.svg#icon"/>
             </svg>
           </div>
           <div contenteditable="true" type="text" class="form-control" v-on:blur="(e) => editTask(e, index)" @keydown.enter="(e) => { e.preventDefault(); e.target.blur(); }" v:model="task.description" >{{ task.description }}</div>
